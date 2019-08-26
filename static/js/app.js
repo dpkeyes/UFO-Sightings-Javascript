f8e8f4668c7ed488e88d1125e6f27e11e2b44b7a
dpkeyes <dpkeyes@gmail.com>
2019-08-26 16:03:35 -0700

    nearly done

--- static/js/app.js
+++ static/js/app.js
@@ -4,18 +4,23 @@ var tableData = data;
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 
-// Populate the table body in index.html with this table data
-data.forEach( function(ufo) {
-    console.log(ufo);
-    var row = tbody.append("tr"); // add a blank row to the table
-    
-    // Set up another for loop over each index, value pair for the ufo element
-    Object.entries(ufo).forEach(function([index, value]) {
-        console.log(key, value);
-        var cell = row.append("td"); // add a blank cell to the active row of the table
-        cell.text(value); // populate that cell with the value of the active index
+// Define a function that populates the table body in index.html
+function populateTable(data) {
+    return data.forEach( function(ufo) {
+        console.log(ufo);
+        var row = tbody.append("tr"); // add a blank row to the table
+        
+        // Set up another for loop over each index, value pair for the ufo element
+        Object.entries(ufo).forEach(function([index, value]) {
+            console.log(index, value);
+            var cell = row.append("td"); // add a blank cell to the active row of the table
+            cell.text(value); // populate that cell with the value of the active index
+        });
     });
-});
+}; 
+
+// Call the function on the full data set so table appears on page load
+populateTable(tableData);
 
 // -------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
@@ -23,7 +28,7 @@ data.forEach( function(ufo) {
 // Use D3 to select the filter button
 var button = d3.select("#filter-btn");
 
-// Write method to initiate new rows being added to the table and then displayed
+// Write method to filter based clicking the filter button + the fill-in field
 button.on("click", function() {
 
   // Select the input element and get the raw HTML node
@@ -35,8 +40,11 @@ button.on("click", function() {
   // Log the input value
   console.log(inputValue);
 
-  // Set the span tag in the h1 element to the text
-  // that was entered in the form
-  d3.select("h1>span").text(inputValue);
+  // Filter the data based on the input value
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+  // Display just the filteredData
+  tbody.html(""); // remove any existing table data
+  populateTable(filteredData); // use populate table function to add filtered data
 });
 
